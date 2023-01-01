8c4b7da328dbbf3aa9804e142f332c24b6bcf603
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-01-01 22:50:55 +0530

    Create hashMaps.java

---
+++ hashMaps.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class hashMaps {
+    public static void main(String[] args) {
+        HashMap<String , Integer> newMap = new HashMap<>();
+        //creating a HashMap 
+        newMap.put("India", 130);
+        newMap.put("China", 150);
+        newMap.put("Indonesi", 10);
+        System.out.println(newMap);
+            
+        
+    }
+}
